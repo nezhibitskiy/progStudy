2d976e6dd0f16f489243b576239731fc38c920e2
Nezhibitskiy <cross01rus@icloud.com>
2021-09-22 15:53:09 +0300

    Added technopark test project

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+ // Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+int rangeSum(TreeNode* root, int low, int high)
+{
+    int summ = 0;
+    if (root->left != nullptr)
+        summ = summ + rangeSum(root->left, low, high);
+    if (root->right != nullptr)
+        summ = summ + rangeSum(root->right, low, high);
+    if (root->val <= high && root->val >= low)
+        summ = summ + root->val;
+    return summ;
+}
+};
+
+
+int main()
+{
+    // std::cout << "Hello World!\n";
+}
+
